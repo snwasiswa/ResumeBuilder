91c1297b2e01acf2b18c4d4e5c865c95ed51fd88
Steve Wasiswa <stevewasiswa@gmail.com>
2023-01-27 13:54:23 -0600

    Add more models for builder and users apps

--- builder/models.py
+++ builder/models.py
@@ -2,24 +2,33 @@ from decimal import Decimal
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+
+from users.models import User
 
 
 # Create your models here.
 
-class Education:
+class Education(models.Model):
     """Model for the education section"""
+
+    owner = models.ForeignKey(User, related_name='education_added', on_delete=models.CASCADE)
     school = models.CharField(blank=False, null=False, max_length=255)
     degree = models.CharField(blank=False, null=False, max_length=255)
     major = models.CharField(blank=False, null=False, max_length=255)
+    minor = models.CharField(blank=True, null=True, max_length=255)
     gpa = models.DecimalField(max_digits=3, decimal_places=2, default=4.00,
                               validators=[MinValueValidator(Decimal(0.00))])
+    scale = models.DecimalField(max_digits=3, decimal_places=2, default=4.00,
+                                validators=[MinValueValidator(Decimal(0.00))])
+    year = models.CharField(blank=False, null=False, max_length=255)
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         verbose_name = 'Education'
         verbose_name_plural = 'Educations'
-
-    def __init__(self):
-        self.slug = None
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.school)
@@ -29,3 +38,114 @@ class Education:
         return self.school
 
 
+class WorkExperience(models.Model):
+    """Model for the education section"""
+    owner = models.ForeignKey(User, related_name='experience_added', on_delete=models.CASCADE)
+    company = models.CharField(blank=False, null=False, max_length=255)
+    position = models.CharField(blank=False, null=False, max_length=255)
+    description_or_role = models.CharField(blank=False, null=False, max_length=255)
+    year = models.CharField(blank=False, null=False, max_length=255)
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = 'WorkExperience'
+        verbose_name_plural = 'WorkExperiences'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.position)
+        super(WorkExperience, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.position
+
+
+class Skill(models.Model):
+    """Model for skills"""
+    owner = models.ForeignKey(User, related_name='skill_added', on_delete=models.CASCADE)
+    name = models.CharField(max_length=25, blank=True, null=True)
+    image = models.FileField(upload_to="logos", null=True, blank=True)
+    rating = models.IntegerField(default=4, null=True, blank=True)
+    is_hard_skill = models.BooleanField(default=False)
+    is_soft_skill = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = 'Skill'
+        verbose_name_plural = 'Skills'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Skill, self).save(*args, **kwargs)
+
+    @property
+    def get_logo_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return "https://res.cloudinary.com/dh13i9dce/image/upload/v1642216413/media/logos/default-thumb_dn1xzg.png"
+
+
+class Leadership(models.Model):
+    """Model for user campus involvement or leadership"""
+    owner = models.ForeignKey(User, related_name='leadership_added', on_delete=models.CASCADE)
+    name = models.CharField(blank=True, null=True, max_length=500)
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(blank=True, null=True)
+    description_or_role = RichTextField(blank=True, null=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = 'Leadership'
+        verbose_name_plural = 'Leaderships'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Leadership, self).save(*args, **kwargs)
+
+
+class Project(models.Model):
+    """Model for user portfolio"""
+    owner = models.ForeignKey(User, related_name='project_added', on_delete=models.CASCADE)
+    name = models.CharField(blank=True, null=True, max_length=250)
+    image = models.ImageField(blank=True, null=True, upload_to="portfolios")
+    is_active = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.CharField(blank=True, null=True, max_length=250)
+    role = RichTextField(blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    is_side_project = models.BooleanField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    year = models.CharField(blank=True, null=True, max_length=70)
+    technology = models.CharField(blank=True, null=True, max_length=1000)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Project, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/project/{self.slug}"
+
+    @property
+    def get_logo_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return "https://res.cloudinary.com/dh13i9dce/image/upload/v1642216413/media/logos/default-thumb_dn1xzg.png"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
+from phonenumber_field.formfields import PhoneNumberField
+from localflavor.us.models import USStateField, USZipCodeField
+from localflavor.us.us_states import STATE_CHOICES
+
+from resume import settings
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    """Model for User"""
+    address = models.CharField("Address line 1", max_length=1024)
+    # zip_code = models.CharField("ZIP / Postal code", max_length=12,)
+    zip_code = USZipCodeField()
+    city = models.CharField("City", max_length=1024,)
+    state = USStateField(choices=STATE_CHOICES)
+    country = CountryField()
+    phone = PhoneNumberField()
+    personal_url1 = models.CharField("Url line 1", max_length=1024, blank=True, null=True)
+    personal_url2 = models.CharField("Url line 2", max_length=1024, blank=True, null=True)
+    personal_url3 = models.CharField("Url line 3", max_length=1024, blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', ]
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
+
+    @property
+    def get_avatar_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+        else:
+            return "https://res.cloudinary.com/dh13i9dce/image/upload/v1642216377/media/avatars/defaultprofile_vad1ub.png"
