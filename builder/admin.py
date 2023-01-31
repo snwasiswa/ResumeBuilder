0b1182ad3fe059fbdc0ae990e30d81446d1b14ca
Steve Wasiswa <stevewasiswa@gmail.com>
2023-01-30 18:06:09 -0600

    Registered new models

--- builder/admin.py
+++ builder/admin.py
@@ -7,23 +7,58 @@ from .models import Education, Project, Skill, Leadership, WorkExperience
 class EducationAdmin(admin.ModelAdmin):
     list_display = ('id', 'school', 'degree', 'major',)
 
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+        obj.save()
+
 
 @admin.register(WorkExperience)
 class WorkExperienceAdmin(admin.ModelAdmin):
     list_display = ('id', 'company', 'position', 'description_or_role',)
+
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+        obj.save()
 
 
 @admin.register(Leadership)
 class LeadershipAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+        obj.save()
+
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'rating')
+
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+        obj.save()
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'is_active', 'year')
     readonly_fields = ('slug',)
+
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = request.user
+        obj.save()
